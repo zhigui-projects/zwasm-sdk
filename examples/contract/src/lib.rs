8ca4098e000ce578da8ae8b8995045b62021a50b
grapebaba <281165273@qq.com>
2018-11-29 14:50:31 +0800

    Add test check
    Signed-off-by: grapebaba <281165273@qq.com>

--- examples/contract/src/lib.rs
+++ examples/contract/src/lib.rs
@@ -1,12 +1,12 @@
 extern crate zwasm_sdk;
 
-pub fn invoke(_payload: *const u8, _payload_len: usize) {
+#[no_mangle]
+pub extern "C" fn invoke(_payload: *const u8, _payload_len: usize) {
     let key = "abc";
+    let key1 = "abc1";
     let value = "xyz".as_bytes();
     zwasm_sdk::set(key, value);
     let out_value = zwasm_sdk::get(key);
-    match String::from_utf8(out_value) {
-        Ok(content) => println!("value {}", content),
-        Err(err) => println!("err {}", err),
-    }
+    let out_value = &out_value[..];
+    zwasm_sdk::set(key1, out_value);
 }
